439f590ea61262c0db801ee9961d1b265f74ccf7
mirii1994 <miri.ignatiev@logz.io>
2022-04-27 21:47:31 +0300

    fix python test

--- tests/tests-logs.py
+++ tests/tests-logs.py
@@ -19,7 +19,7 @@ class TestHelmLogsIntegration(unittest.TestCase):
                 "bool": {
                     "must": [{
                         "query_string": {
-                            "query": query
+                            "query": self.query
                         }
                     },
                         {
