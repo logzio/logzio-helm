d491291d1e0aa1e14b05fb2e4c4a53afdbf20eed
tamir-michaeli <tamir.michaeli@logz.io>
2022-11-10 12:28:57 +0200

    removed unecessary files

--- charts/fluentd/app_type_detector/api/v1alpha1/appdetector_types.go
+++ charts/fluentd/app_type_detector/api/v1alpha1/appdetector_types.go
@@ -14,8 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// TODO we dont need intrumented app information?
-
 package v1alpha1
 
 import (
