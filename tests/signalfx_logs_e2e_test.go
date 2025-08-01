3b6b82e94ea38548ea980a1095be294c036d59a1
ralongit <ralgotmail@gmail.com>
2025-08-02 00:20:13 +0300

    fix signalfx test

--- tests/signalfx_logs_e2e_test.go
+++ tests/signalfx_logs_e2e_test.go
@@ -139,7 +139,7 @@ func fetchSignalFxLogs(logsApiKey string) (*LogResponse, error) {
 	url := fmt.Sprintf("%s/search", BaseLogzioApiUrl)
 	client := &http.Client{}
 	envID := os.Getenv("ENV_ID")
-	query := fmt.Sprintf("env_id:%s AND type:agent-k8s AND source:signalfx-logs-gen", envID, envID)
+	query := fmt.Sprintf("env_id:%s AND type:agent-k8s AND source:signalfx-logs-gen", envID)
 	formattedQuery := formatQuery(query)
 	logger.Info("sending api request", zap.String("url", url), zap.String("query", query))
 	req, err := http.NewRequest("POST", url, bytes.NewBufferString(formattedQuery))
